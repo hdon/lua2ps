53f48b409e32a53f3061917b702de0d33630514a
Donny Viszneki <don@codebad.com>
2014-09-22 11:45:57 -0700

    getdeps.sh should now work

--- getdeps.sh
+++ getdeps.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # These deps are pretty small
-git clone https://github.com/lua/lpeg.git lpeg
+git clone https://github.com/lua/lpeg.git lpeg &&
+pushd lpeg && 
+make -f makefile &&
+popd &&
 git clone https://github.com/andremm/lua-parser.git lua-parser
 # To run lua2ps remember these environs:
 echo Dependencies got. Edit your runtests.sh with these environs:
-LUA_CPATH='/mnt/oih/hdon/src/hg/lua2ps/lpeg/?.so'
-LUA_PATH='/mnt/oih/hdon/src/hg/lua2ps/?.lua;/mnt/oih/hdon/src/hg/lua2ps/lua-parser/?.lua'
+echo export LUA_CPATH=\'`pwd`/lpeg/\?.so\'
+echo export LUA_PATH=\'`pwd`/\?.lua\;`pwd`/lua-parser/\?.lua\'
